708a88d75b70e9c770dc7e51d62a574d6a17d237
Thomas Hallock <github@thomashallock.com>
2012-04-19 20:21:48 -0400

    here it is

---
+++ jquery.textWithLineBreaks.js
@@ -0,0 +1,31 @@
+/*global jQuery,window*/
+(function ($) {
+    'use strict';
+    $.fn.textWithLineBreaks = function () {
+        var onnewline = true,
+            f = function (n) {
+                var ret = "";
+                $(n).contents().each(function (i, c) {
+                    var content = "";
+                    if (c.nodeType === window.Node.TEXT_NODE) {
+                        content = $(c).text();
+                        onnewline = false;
+                    } else {
+                        if ($(c).is("br")) {
+                            content = "\n";
+                            onnewline = true;
+                        } else {
+                            if ("block" === $(c).css('display') && !onnewline) {
+                                content = "\n";
+                                onnewline = true;
+                            }
+                            content = content + f(c);
+                        }
+                    }
+                    ret = ret + content;
+                });
+                return ret;
+            };
+        return f(this.first());
+    };
+}(jQuery));
